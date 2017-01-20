28abf99c06bd1c1f459501491303afc04645a534
charmoniumQ <sam@samgrayson.me>
2017-01-20 12:59:57 -0600

    wrote animator

---
+++ animator/main.js
@@ -0,0 +1,69 @@
+// utils
+function last(arr) { return arr[arr.length - 1]; }
+
+// Settings
+const settings = {
+	color: "green",
+	fps: 1.0,
+	car_width: 20,
+	car_length: 35,
+	frames: cars[0].length - 1,
+	dist_max: last(last(cars)),
+};
+
+// draw utilities
+function drawCar(context, instant, settings) {
+	// each frame computes its own drawCar
+	return function(positions) {
+		const pos =
+  			  positions[instant.fmod] * (1 - instant.weight) +
+  			  positions[instant.fmod + 1] * instant.weight;
+		context.fillRect(
+			pos / settings.dist_max * (settings.width - 1) + 1, 1,
+			settings.car_length, settings.car_width);
+	}
+}
+
+function drawRoad(context, instant, settings) {
+	context.strokeRect(0, 0, settings.width, settings.car_width);
+}
+
+function drawTime(context, instant, settings) {
+	context.fillRect(0, settings.car_width + 3,
+					 instant.phase * settings.width, 1);
+	const text = "frame: " + (instant.fmod + instant.weight).toFixed(2);
+	context.fillText(text, 0, settings.car_width + 20)
+}
+
+function draw() {
+	// compute frame
+	const s = new Date().getTime() / 1e3;
+	const f = Math.floor(s * settings.fps);
+	const weight = s - f / settings.fps;
+	const fmod = f % (settings.frames - 1);
+	const phase = (fmod + weight) / (settings.frames - 1);
+	const instant = {fmod: fmod, weight: weight, phase: phase};
+
+	// prepare canvas
+	const canvas = document.getElementById("canvas");
+	const context = canvas.getContext("2d");
+	canvas.width = canvas.clientWidth;
+	
+	settings.width = canvas.width;
+	settings.height = canvas.height;
+	// as per http://stackoverflow.com/questions/15661339/how-do-i-fix-blurry-text-in-my-html5-canvas#15666143
+	canvas.style.width = canvas.width;
+	canvas.style.height = canvas.height;
+	context.fillStyle = settings.color;
+	context.clearRect(0, 0, settings.width, settings.height);
+
+	// draw stuff
+	drawRoad(context, instant, settings);
+	drawTime(context, instant, settings);
+	cars.map(drawCar(context, instant, settings));
+	
+	// call self
+	window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
